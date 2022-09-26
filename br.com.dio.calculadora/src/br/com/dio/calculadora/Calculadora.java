6aa3a403f1c563b4133e909baa17253931107b1c
Vitor Oliveira <113632825+VitornLnT@users.noreply.github.com>
2022-09-25 21:33:48 -0300

    Add files via upload

---
+++ br.com.dio.calculadora/src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,54 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+	
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		double divisao = divisao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		
+		System.out.println("Soma " + soma);
+		System.out.println("Subtracao " + subtracao);
+		System.out.println("Divisao " + divisao);
+		System.out.println("Multiplicacao " + multiplicacao);
+		
+		
+		
+		
+		
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	
+	}
+	public static int subtracao(int a, int b) {
+		return a - b;
+		
+	}
+	public static double divisao(int a, int b) {
+		return a / b;
+		
+	}
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+		
+	}
+
+}
